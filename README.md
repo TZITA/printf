91f65fd8948389dd9c4fb6c67c50c918d1f691fe
Thomas Beyene <thomas.beyene.987@gmail.com>
2022-07-11 12:37:39 -0700

    Tasks 0 and 1

---
+++ README.md
@@ -0,0 +1,7 @@
+Project on the implementation of printf function as part of the alx SE program.
+
+_printf
+
+A function that formats and prints arguments to the output similar to the C standard library function, printf.
+
+
---
+++ main.h
@@ -0,0 +1,93 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int _printf(const char *format, ...);
+char *convert(unsigned int num, int base);
+
+/**
+ * _printf - prints and formats data
+ * @format: items to be printed
+ *
+ * Return: Integer.
+ */
+
+int _printf(const char *format, ...)
+{
+	const char *toprint;
+	int i;
+	char *s;
+	va_list arg;
+
+	va_start(arg, format);
+
+	for (toprint = format; *toprint; toprint++)
+	{
+		if (*toprint != '%')
+		{
+			putchar(*toprint);
+			continue;
+		}
+		switch (*++toprint)
+		{
+			case 'c':
+				i = va_arg(arg, int);
+				putchar(i);
+				break;
+			case 'd':
+				i = va_arg(arg, int);
+				if (i < 0)
+				{
+					i = -i;
+					putchar('-');
+				}
+				fputs((convert(i, 10)), stdout);
+				break;
+			case 'i':
+				i = va_arg(arg, int);
+				if (i < 0)
+				{
+					i = -i;
+					putchar('-');
+				}
+				fputs((convert(i, 10)), stdout);
+				break;
+			case 's':
+				for (s = va_arg(arg, char *); *s; s++)
+					putchar(*s);
+				break;
+			default:
+				putchar(*toprint);
+				break;
+		}
+		va_end(arg);
+	}
+	return (0);
+}
+
+/**
+ * convert - converts integer to the specified base
+ * @num: number
+ * @base: base
+ *
+ * Return: Pointer.
+ */
+
+char *convert(unsigned int num, int base)
+{
+	char rep[] = "0123456789ABCDEF";
+	static char tmp[50];
+	char *p;
+
+	p = &tmp[49];
+	*p = '\0';
+	do {
+		*--p = rep[num % base];
+		num /= base;
+	} while (num != 0);
+	return (p);
+}
+
+#endif
